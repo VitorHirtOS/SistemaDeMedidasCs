702820f907c6bbd6c3eef4a1f4aa87f92c6fb812
Vitor Hirt <123315244+VitorHirtOS@users.noreply.github.com>
2023-08-12 19:36:10 -0300

    Delete FunctionSystem.h

--- SistemaDeMedidasCs/FunctionSystem.h
+++
@@ -1,7 +0,0 @@
-// Protótipos
-
-void DollarEuro();
-double EscolhaConvert();
-std::string RetornoOpcao(std::string acao, double moeda);
-double RealBrazil(int opcaoReal, double brazilReal);
-double ConversaoBitcoin(double valueC);
